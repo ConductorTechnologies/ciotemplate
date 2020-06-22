310db8ab3416dbfa6acf148d6fd8b1430e671328
Julian Mann <julian.mann@gmail.com>
2020-06-22 00:47:28 -0500

    remove src

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@
 import os
 import re
 import setuptools
+from setuptools.command.build_py import build_py
+
 
 NAME = "ciocore"
 DESCRIPTION = "Core functionality for Conductor's client tools"
@@ -14,8 +16,17 @@ REQUIRES_PYTHON = "~=2.7"
 REQUIRED = ["pyjwt>=1.4.2", "pyyaml>=3.11", "requests>=2.10.0"]
 HERE = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(HERE, "src", NAME, 'VERSION')) as version_file:
+with open(os.path.join(HERE,'VERSION')) as version_file:
     VERSION = version_file.read().strip()
+
+class BuildCommand(build_py):
+    def run(self):
+        build_py.run(self)
+
+        if not self.dry_run:
+            with open(os.path.join(self.build_lib, NAME, "VERSION"), "w") as f:
+                f.write(VERSION)
+ 
 
 setuptools.setup(
     author=AUTHOR,
@@ -26,6 +37,7 @@ setuptools.setup(
         "Programming Language :: Python",
         "Topic :: Multimedia :: Graphics :: 3D Rendering",
     ],
+    cmdclass={"build_py": BuildCommand},
     description=DESCRIPTION,
     entry_points={"console_scripts": ["conductor=conductor.cli.conductor:main"]},
     include_package_data=True,
@@ -33,10 +45,11 @@ setuptools.setup(
     long_description=DESCRIPTION,
     long_description_content_type="text/markdown",
     name=NAME,
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
     python_requires=REQUIRES_PYTHON,
     url=URL,
     version=VERSION,
     zip_safe=False,
+
 )
