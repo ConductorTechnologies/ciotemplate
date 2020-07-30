4cf54d52e32e5b2e3a51520a2f3c249460675761
Julian Mann <julian.mann@gmail.com>
2020-07-30 18:42:15 -0500

    data must be initialized with software product.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
- 
+
 import os
 import re
 import setuptools
@@ -13,11 +13,13 @@ URL = "https://github.com/AtomicConductor/conductor-core"
 EMAIL = "info@conductortech.com"
 AUTHOR = "conductor"
 REQUIRES_PYTHON = "~=2.7"
-REQUIRED = ["pyjwt>=1.4.2", "pyyaml>=3.11", "requests>=2.10.0", "future>=0.18.0"]
+REQUIRED = ["pyjwt>=1.4.2", "pyyaml>=3.11",
+            "requests>=2.10.0", "future>=0.18.0"]
 HERE = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(HERE,'VERSION')) as version_file:
+with open(os.path.join(HERE, 'VERSION')) as version_file:
     VERSION = version_file.read().strip()
+
 
 class BuildCommand(build_py):
     def run(self):
@@ -26,7 +28,7 @@ class BuildCommand(build_py):
         if not self.dry_run:
             with open(os.path.join(self.build_lib, NAME, "VERSION"), "w") as f:
                 f.write(VERSION)
- 
+
 
 setuptools.setup(
     author=AUTHOR,
@@ -39,7 +41,8 @@ setuptools.setup(
     ],
     cmdclass={"build_py": BuildCommand},
     description=DESCRIPTION,
-    entry_points={"console_scripts": ["conductor=ciocore.cli.conductor:main", "setup=ciocore.cli.setup:main"]},
+    entry_points={"console_scripts": [
+        "conductor=ciocore.cli.conductor:main", "setup=ciocore.cli.setup:main"]},
     include_package_data=True,
     install_requires=REQUIRED,
     long_description=DESCRIPTION,
